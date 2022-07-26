6c891079277aee431918e2cd6cf910146f0779b6
Lisa Williams <lisaismyname0@gmail.com>
2022-07-26 18:59:27 -0400

    page very VERY rudimentarily shows that I'm following carlos

--- social_ecards/src/components/loginPage.js
+++ social_ecards/src/components/loginPage.js
@@ -4,14 +4,39 @@ import { useState, useEffect } from "react";
 export function UserList({ authToken }) {
   const [users, setUsers] = useState([]);
   const [admin, setAdmin] = useState("");
+  const [lisa, setLisa] = useState("");
+  const [isFollowing, setIsFollowing] = useState([]);
 
   useEffect(() => {
     getUsers(authToken).then((res) => {
       setUsers(res.USERS);
       setAdmin(res.USERS[0]);
-      console.log(admin);
+      setLisa(res.USERS[1]);
+      let lisaIsFollowing = res.USERS[1].is_following;
+      console.log(lisaIsFollowing);
+      setIsFollowing(lisaIsFollowing);
     });
   }, [authToken]);
 
-  return <>{users && admin && <p>Hello, {admin.username}</p>}</>;
+  return (
+    <>
+      {users && admin && lisa && (
+        <>
+          <article>Hello, {admin.username}</article>
+          <article>
+            Hello, {lisa.username}
+            <ul>Following</ul>
+            {isFollowing && (
+              <li>
+                {isFollowing.username}
+                {/* {isFollowing.map((user) => (
+                  <p>{user.username}</p>
+                ))} */}
+              </li>
+            )}
+          </article>
+        </>
+      )}
+    </>
+  );
 }
