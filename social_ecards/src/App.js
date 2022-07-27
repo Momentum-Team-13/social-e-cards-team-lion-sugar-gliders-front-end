961337b1c735ecbc925cf2c1a9c6fb784dbed817
Lisa Williams <lisaismyname0@gmail.com>
2022-07-27 10:56:10 -0400

    logging in admin returns auth_token

--- social_ecards/src/App.js
+++ social_ecards/src/App.js
@@ -1,23 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 import AddUserForm from "./components/add_user_form";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { UserList } from "./components/loginPage";
 import LogInForm from "./components/login_form.js";
-
-// import {
-//   InputField,
-//   LoginForm,
-//   RefInput,
-//   BookList,
-// } from "./components/practice_forms";
+import axios from "axios";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [baseURL, setBaseURL] = useState("https://sg-ecard-api.herokuapp.com/");
+
   return (
     <div className="App">
       <h1>Team Lion Sugar Gliders Social E-Cards App!</h1>
-      <>
+      {/* <>
         <span>
           click <a href="google.com">HERE </a> to log in |
         </span>
@@ -25,10 +21,10 @@ function App() {
         <span>
           | click <a href="./add_user_form">HERE</a> to create a username
         </span>
-      </>
-      <LogInForm />
-      {/* <AddUserForm /> */}
+      </> */}
 
+      <LogInForm baseURL={baseURL} />
+      {/* <AddUserForm /> */}
       {/* <button onClick={(e) => setLoggedIn(true)}> Log Me In</button> */}
       <UserList />
     </div>
--- social_ecards/src/components/login_form.js
+++ social_ecards/src/components/login_form.js
@@ -1,19 +1,23 @@
+import axios from "axios";
 import { useState } from "react";
 
-export default function LoginForm() {
+export default function LoginForm({ baseURL }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [authToken, setAuthToken] = useState("");
 
   const LogIn = (event) => {
     event.preventDefault();
     // when form submits, make an AJAX request to login endpoint and when that happens, capture the login auth token in state --> i do this here bc this is what renders the form. when i get this info i want to pass it into app
-    console
-      .log("login attempt")
-      .post("http://localhost:3000/", {
+    axios
+      .post(`${baseURL}auth/token/login`, {
         username: username,
         password: password,
       })
-      .then((res) => console.log(res));
+      .then((res) => {
+        let auth_token = res.data.auth_token;
+        setAuthToken(auth_token);
+      });
   };
 
   return (
