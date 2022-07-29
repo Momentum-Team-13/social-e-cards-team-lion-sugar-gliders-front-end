1e33129faad19bd1bddc305ce0d1076d30661aa9
Carlos <carlosaltirado@gmail.com>
2022-07-29 15:21:34 -0500

    need to fix the show more buttons

---
+++ social_ecards/src/Components/Cardscomponent.js
@@ -0,0 +1,33 @@
+// import App from "../App";
+// import './App.css';
+// import React from "react";
+// import Slider from 'react-slick';
+// import "slick-carousel/slick/slick.css"; 
+// import "slick-carousel/slick/slick-theme.css";
+// import { listCards } from './data';
+
+
+// function ShowCards() {
+//     return (
+//         <div className="container">
+//             {listCards.map((item) => (
+//             <div className="card">
+//                 <div className="card-top">
+//                     <img src={item.coverImageUrl} alt={item.title}/>
+//                     <h1>{item.title}</h1>
+//                     </div>  
+//                 <div className="card-bottom">
+//                 <h3>{item.author}</h3>
+//                 <p>{item.publicationDate}</p>
+//                 </div>
+//             </div>
+//             ))}
+//         </div>
+//         )
+// }
+
+
+// export default ShowCards
+
+
+
---
+++ social_ecards/src/data.js
@@ -0,0 +1,99 @@
+export const listCards = [
+    
+        {
+        id: 1,
+        title: "Princess Mononoke",
+        coverImageUrl: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/jHWmNr7m544fJ8eItsfNk8fs2Ed.jpg",
+        author: "Hayao Miyazaki",
+        publicationDate: "1997",
+        },
+
+        {
+        id: 2,
+        title: "My Neighbor Totoro",
+        author: "Hayao Miyazaki",
+        coverImageUrl: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/rtGDOeG9LzoerkDGZF9dnVeLppL.jpg",
+        publicationDate: "1988",
+        },
+    
+        {
+        id: 3,
+        title: "Howl's Moving Castle",
+        author: "Hayao Miyazaki",
+        coverImageUrl: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/TkTPELv4kC3u1lkloush8skOjE.jpg",    
+        publicationDate: "2004",
+        },
+
+        {
+        id: 4,
+        title: "Spirited Away",
+        author: "Hayao Miyazaki",
+        coverImageUrl: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/39wmItIWsg5sZMyRUHLkWBcuVCM.jpg",
+        publicationDate: "2001",
+        },
+
+        {
+        id: 5,
+        title: "My Neighbor Totoro",
+        author: "Hayao Miyazaki",
+        coverImageUrl: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/rtGDOeG9LzoerkDGZF9dnVeLppL.jpg",
+        publicationDate: "1988",
+        },
+
+        {
+        id: 6,
+        title: "Howl's Moving Castle",
+        author: "Hayao Miyazaki",
+        coverImageUrl: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/TkTPELv4kC3u1lkloush8skOjE.jpg",    
+        publicationDate: "2004",
+        },
+
+        {
+        id: 7,
+        title: "Princess Mononoke",
+        coverImageUrl: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/jHWmNr7m544fJ8eItsfNk8fs2Ed.jpg",
+        author: "Hayao Miyazaki",
+        publicationDate: "1997",
+        },
+
+        {
+        id: 8,
+        title: "Spirited Away",
+        author: "Hayao Miyazaki",
+        coverImageUrl: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/39wmItIWsg5sZMyRUHLkWBcuVCM.jpg",
+        publicationDate: "2001",
+        },
+    
+        {
+        id: 9,
+        title: "Spirited Away",
+        author: "Hayao Miyazaki",
+        coverImageUrl: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/39wmItIWsg5sZMyRUHLkWBcuVCM.jpg",
+        publicationDate: "2001",
+        },
+
+        {
+        id: 10,
+        title: "Howl's Moving Castle",
+        author: "Hayao Miyazaki",
+        coverImageUrl: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/TkTPELv4kC3u1lkloush8skOjE.jpg",    
+        publicationDate: "2004",
+        },
+
+        {
+            id: 11,
+            title: "You Don't Know JS Yet",
+            author: "Kyle Simpson",
+            coverImageUrl: "https://github.com/getify/You-Dont-Know-JS/raw/2nd-ed/get-started/images/cover.png",
+            publicationDate: "Jan 2020",
+        },
+        {
+            id: 12,
+            title: "You Don't Know JS Yet",
+            author: "Kyle Simpson",
+            coverImageUrl: "https://github.com/getify/You-Dont-Know-JS/raw/2nd-ed/get-started/images/cover.png",
+            publicationDate: "Jan 2020",
+        }
+
+    ]
+
