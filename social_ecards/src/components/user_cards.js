c4da0bc8c74944cf3b6d5379a91d4da842119118
Lisa Williams <lisaismyname0@gmail.com>
2022-07-31 18:50:51 -0400

    all cards view mapping through all cards. clicking on a cards div will console log the cards id in attempt to use that number as the int pk for editing purposes

--- social_ecards/src/components/user_cards.js
+++ social_ecards/src/components/user_cards.js
@@ -3,25 +3,85 @@ import axios from "axios";
 import { baseURL } from "../helpers/constants";
 
 export default function AllCards({ token, username }) {
-  const [cardData, setCardData] = useState([]);
+  const [totalCards, setTotalCards] = useState(0);
+  const [cardIndex, setCardIndex] = useState(0);
 
-  useEffect = () => {
-    axios
-      .get(`${baseURL}ecards/`, {
-        headers: { Authorization: `Token ${token}` },
-      })
-      .then((res) => {
-        console.log(res.data);
-        const CARDS = res.data;
-        setCardData(CARDS);
-      })
-      .catch((res) => console.log(res));
-  };
+  // useEffect = () => {
+  axios
+    .get(`${baseURL}ecards/`, {
+      headers: { Authorization: `Token ${token}` },
+    })
+    .then((res) => {
+      console.log(res);
+      let total = res.data.length;
+      setTotalCards(total);
+      // const CARDS = res.data;
+      // console.log(CARDS.map((card) => card.id));
+      // setCardData(CARDS);
+    }, [])
+    .catch((res) => console.log(res));
+  // };
 
   return (
     <div>
       <h1>See all of {username}'s cards</h1>
+      <div>
+        <Index username={username} totalCards={totalCards} />
+        <CardList token={token} />
+      </div>
       <div></div>
     </div>
   );
 }
+
+const Index = ({ username, totalCards }) => {
+  return (
+    <>
+      <div>
+        {username} has {totalCards} total cards
+      </div>
+    </>
+  );
+};
+
+const CardList = ({ token }) => {
+  const [cardData, setCardData] = useState([]);
+
+  axios
+    .get(`${baseURL}ecards/`, {
+      headers: { Authorization: `Token ${token}` },
+    })
+    .then((res) => {
+      const CARDS = res.data;
+      setCardData(CARDS);
+    }, [])
+    .catch((res) => console.log(res));
+
+  const EditCard = () => {};
+
+  return (
+    <div>
+      <div>
+        {cardData.map((card) => (
+          <div
+            className="card"
+            key={card.id}
+            id={card.id}
+            onClick={(e) => console.log(e.target.id)}
+          >
+            <h1>{card.card_outer_message}</h1>
+            <h2>{card.card_inner_message}</h2>
+            <p>Card created at: {card.created_at}</p>
+            <p>Card updated at: {card.updated_at}</p>
+            <img src={card.card_image} alt="place kitten card cover" />
+
+            <div>
+              <button onClick={() => EditCard()}>Edit Card</button>
+            </div>
+          </div>
+        ))}
+        ;
+      </div>
+    </div>
+  );
+};
